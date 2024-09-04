c715a8cbe412f91fa68a630bf480d878925cbc2b
evert <evert@everbuild.be>
2024-09-05 01:06:15 +0200

    base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/logard-demo',
   plugins: [vue()],
   resolve: {
     extensions: ['.js', '.ts', '.vue'],
